e55a4d5d8c8af38fb7c923263f93c17148dd5654
hbvj99 <helloblazevijay99@gmail.com>
2020-06-17 21:10:06 +0545

    fix pagination, discussion text

--- discussions/discussion_api.py
+++ discussions/discussion_api.py
@@ -87,31 +87,29 @@ class DiscussionRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
 
     def update(self, request, slug, *args, **kwargs):
         post = Discussion.objects.get(slug=slug)
-        update_usr = request.user
 
-        # print(post.id)
-        # print(update_usr.id)
-        # print(request.data.get('user_id'))
-        
-        if post.user != update_usr:
+        print(post.user.id)
+        print(request.data.get('user_id'))
+        print(request.user.id)
+
+        if post.user.id and request.user.id != request.data.get('user_id'):
             return Response({'response': "You don't have permission to edit this discussion."})
-        else:
-            response = super().update(request, *args, **kwargs)
-            if response.status_code == 200:
-                from django.core.cache import cache
-                discussion = response.data
-                cache.set('discussion_data_{}'.format(discussion['slug']), {
-                    'user': discussion['user'],
-                    'title': discussion['title'],
-                    'updated': discussion['updated'],
-                    'timestamp': discussion['timestamp'],
-                    'content': discussion['content'],
-                    'img': discussion['img'],
-                    'category': discussion['category'],
-                    'tags': discussion['tags'],
-                    'views': discussion['views'],
-                    'votes': discussion['votes'],
-                })
-            return response
+        response = super().update(request, *args, **kwargs)
+        if response.status_code == 200:
+            from django.core.cache import cache
+            discussion = response.data
+            cache.set('discussion_data_{}'.format(discussion['slug']), {
+                'user': discussion['user'],
+                'title': discussion['title'],
+                'updated': discussion['updated'],
+                'timestamp': discussion['timestamp'],
+                'content': discussion['content'],
+                'img': discussion['img'],
+                'category': discussion['category'],
+                'tags': discussion['tags'],
+                'views': discussion['views'],
+                'votes': discussion['votes'],
+            })
+        return response
 
 
