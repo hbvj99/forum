aad2e19fb49d773c4161906041e11d7bf2c9ecea
hbvj99 <helloblazevijay99@gmail.com>
2020-07-19 19:47:46 +0545

    [m]: Adds env template

---
+++ forum/example.env.py
@@ -0,0 +1,20 @@
+# Keys, Debug, Host
+SECRET_KEY = ''
+DEBUG = True
+ALLOWED_HOSTS = ['*', ]
+
+# DB
+DB_NAME = ''
+DB_USER = ''
+DB_PASSWORD = ''
+DB_HOST = 'localhost'  # localhost default
+DB_PORT = ''  # postgres port 5432
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = ''
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = ''
+EMAIL_USE_TLS = True
